ef358cfd6e370138eba4632c4fcbe295bed0f19c
ajay.chaubal.op <ajay.chaubal@op.fi>
2022-02-03 13:42:28 +0200

    changed message slightly v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Everyone! This line is changed slightly'));
+app.get('/', (req, res) => res.send('Hello Everyone! Second change to verify all stages are working fine'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
